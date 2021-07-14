91921515425f4ef156062095d6c6a55c4c680ee8
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-07-15 02:05:39 +0530

    Update Type_conversion.py

--- Type_conversion.py
+++ Type_conversion.py
@@ -2,8 +2,10 @@ two_digit_number = input("Type a two digit number: ")
 
 print(type(two_digit_number))
 
-# first_digit = two_digit_number[0]
-# second_digit = two_digit_number[1]
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
 
-print(int(two_digit_number[0]) + int(two_digit_number[1]))
+result = int(first_digit) + int(second_digit)
+
+print(result)
 
