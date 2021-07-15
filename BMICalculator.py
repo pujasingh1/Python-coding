f955ecc0e3e7b84bbe1d1f0f4970f065e05fcd1c
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-07-16 00:51:00 +0530

    Create BMICalculator.py
    This is a program that calculates the Body Mass Index (BMI) from a user's weight and height.

---
+++ BMICalculator.py
@@ -0,0 +1,8 @@
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+height_as_float = height
+weight_as_float = weight
+
+BMI = weight/floatheight ** 2
+
+print(int(BMI))
