7e56d668796bf4ee5b2451a4a1338b8711cb8b17
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-07-27 00:51:21 +0530

    Update love_calculator.py

--- love_calculator.py
+++ love_calculator.py
@@ -6,21 +6,15 @@ merge_of_name = (name1+ " " +name2)
 lower_case_name = merge_of_name.lower()
 
 t = lower_case_name.count("t")
-
 r = lower_case_name.count("r")
-
 u = lower_case_name.count("u")
-
 e = lower_case_name.count("e")
 
 Total1 =(t + r + u + e)
 
 l = lower_case_name.count("l")
-
 o = lower_case_name.count("o")
-
 v = lower_case_name.count("v")
-
 e = lower_case_name.count("e")
 
 Total2 = (l + o + v + e)
