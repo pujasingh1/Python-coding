22ade5637c9fc68f42af71868880e232f2c0019b
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-08-10 00:31:16 +0530

    Update Hangman_start.py

--- Hangman_start.py
+++ Hangman_start.py
@@ -1,20 +1,24 @@
 import random
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
 
 print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
-word_length = len(chosen_word)
-for each_letter in range(word_length):
-  display += ('_')
+for _ in range(word_length):
+    display += "_"
 
+end_of_game = False
+while not end_of_game:
+  guess = input("Guess a letter: ").lower()
 
-guess = input("Guess a letter: ").lower()
+  for position in range(word_length):
+      letter = chosen_word[position]
+      if letter == guess:
+          display[position] = letter
 
-for position in range(word_length):
-  letter = chosen_word[position]
-  if letter == guess:
-    display[position] = letter
-  
-print(display)
+  print(display)
+  if "_" not in display:
+    end_of_game = True
+    print("You win!")
