7ff6c0a6e919b12ca7ac5ad0d101687805f346f6
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-09-03 10:28:33 +0530

    Update travel_log_dictionary.py

--- travel_log_dictionary.py
+++ travel_log_dictionary.py
@@ -10,10 +10,6 @@ travel_log = [
   "cities": ["Berlin", "Hamburg", "Stuttgart"]
 },
 ]
-#🚨 Do NOT change the code above
-
-#TODO: Write the function that will allow new countries
-#to be added to the travel_log. 👇
 def add_new_country(country_visited, times_visited, cities_visited):
   new_country = {}
   new_country["country"] = country_visited
@@ -21,7 +17,5 @@ def add_new_country(country_visited, times_visited, cities_visited):
   new_country["cities"] = cities_visited
 
   travel_log.append(new_country)
-
-#🚨 Do not change the code below
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
