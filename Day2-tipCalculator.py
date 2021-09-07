6bec1e7ffcb662ab8950071eb1f512e037f2ddc9
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-09-08 00:03:17 +0530

    Update Day2-tipCalculator.py

--- Day2-tipCalculator.py
+++ Day2-tipCalculator.py
@@ -1,5 +1,4 @@
 print("Welcome to the tip calculator")
-
 bill = float(input("What was the total bill? Rs"))
 tip = int(input("What percentage tip would you like to give? 10, 12 or 15? "))
 people = int(input("How many people to split the bill? "))
