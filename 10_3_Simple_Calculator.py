6db7a2b22c8494582466c9ad9a34d9a175c8eb4b
Puja Singh <52859941+pujasingh1@users.noreply.github.com>
2021-09-20 00:49:16 +0530

    Update 10_3_Simple_Calculator.py

--- 10_3_Simple_Calculator.py
+++ 10_3_Simple_Calculator.py
@@ -1,15 +1,12 @@
 #Add 
 def add(n1, n2):
   return n1 + n2
-
 #Subtract
 def subtract(n1, n2):
   return n1 - n2
-
 #Multiply
 def multiply(n1, n2):
   return n1 * n2
-
 #Division
 def divide(n1, n2):
   return n1 / n2
